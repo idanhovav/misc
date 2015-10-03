aabb36f92474483578bb121a4348dc58016cfa2f
Idan <idanhovav@users.noreply.github.com>
2015-10-02 18:23:38 -0700

    Third commit.

---
+++ Game/Game/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Game
+//
+//  Created by Idan Hovav on 9/30/15.
+//  Copyright © 2015 Idan Hovav. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Game/Game/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Idan Hovav on 9/30/15.
+//  Copyright © 2015 Idan Hovav. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
