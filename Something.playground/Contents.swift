cdf55e07aac28c16e2922bc7fb24252e8603aaa4
Idan <idanhovav@users.noreply.github.com>
2015-10-02 18:21:29 -0700

    First commit.

---
+++ Something.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+func main(){
+    let str = "Hello, playground"
+    var i = 5
+    while (i < 10){
+        i = i + 1
+        print(i)
+        print(str)
+    }
+}
+
+func swapper(a: Int, b: Int) -> Int{
+    if(a == 5){
+        return 2
+    }
+    else{
+        return a + 3
+    }
+}
+    
+main()
+var word = "jumanji"
+print(word)
+print(word)
+word = word + "ll"
+print(word)
+let swan = swapper(1, b: 4)
+print(swan)
